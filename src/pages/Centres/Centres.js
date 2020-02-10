56c6239062c69076a6ff7ced1938436804c1a331
Stephen <stephen.sjbrown@gmail.com>
2020-02-10 18:43:43 +0800

    WIP start of nearest centre

---
+++ src/pages/Centres/Centres.js
@@ -0,0 +1,21 @@
+import React, {useState} from "react";
+
+const Centres = () => {
+
+    // API ADDRESS:
+    // https://maps.googleapis.com/maps/api/place/findplacefromtext/json?key={}&input=recycling+centre&inputtype=textquery&language=en-GB&fields=formatted_address,geometry,icon,name,permanently_closed,place_id,plus_code,types&locatiobias=ipbias
+
+    // https://www.google.com/maps/place/?q=place_id:ChIJ6eTsxytPzDERW07hq6W8cBA
+
+    const [placeID, setPlaceID] = useState('')
+    const [name, setName] = useState('')
+
+  return (
+    <>
+      <h1>Centres</h1>
+      <p>Your nearest recycling centre is: </p>
+  <h2>{name}</h2>
+      <a href="https://www.google.com/maps/place/?q=place_id:{placeID}">
+    </>
+  );
+};
