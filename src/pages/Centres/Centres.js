eec2da7191de7264a30ae15b2afb24a8c4f892aa
Stephen <stephen.sjbrown@gmail.com>
2020-02-11 11:01:42 +0800

    WIP displaying coupons

--- src/pages/Centres/Centres.js
+++ src/pages/Centres/Centres.js
@@ -1,21 +1,24 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
+import LoadingIndicator from '../../components/LoadingIndicator'
 
 const Centres = () => {
+  // API ADDRESS:
+  // https://maps.googleapis.com/maps/api/place/findplacefromtext/json?key={}&input=recycling+centre&inputtype=textquery&language=en-GB&fields=formatted_address,geometry,icon,name,permanently_closed,place_id,plus_code,types&locatiobias=ipbias
 
-    // API ADDRESS:
-    // https://maps.googleapis.com/maps/api/place/findplacefromtext/json?key={}&input=recycling+centre&inputtype=textquery&language=en-GB&fields=formatted_address,geometry,icon,name,permanently_closed,place_id,plus_code,types&locatiobias=ipbias
+  // https://www.google.com/maps/place/?q=place_id:ChIJ6eTsxytPzDERW07hq6W8cBA
 
-    // https://www.google.com/maps/place/?q=place_id:ChIJ6eTsxytPzDERW07hq6W8cBA
+  const [placeID, setPlaceID] = useState("");
+  const [name, setName] = useState("");
 
-    const [placeID, setPlaceID] = useState('')
-    const [name, setName] = useState('')
 
   return (
     <>
       <h1>Centres</h1>
       <p>Your nearest recycling centre is: </p>
-  <h2>{name}</h2>
+      <h2>{name}</h2>
       <a href="https://www.google.com/maps/place/?q=place_id:{placeID}">
+        Go there
+      </a>
     </>
   );
 };
