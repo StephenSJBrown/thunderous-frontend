72d1f934610350a2c1e6672a67494ca49c870108
Stephen <stephen.sjbrown@gmail.com>
2020-02-11 18:55:37 +0800

    WIP start

--- src/pages/Deposits/Deposit.js
+++ src/pages/Deposits/Deposit.js
@@ -1,11 +1,34 @@
-import React from "react";
+import React, {useState} from "react";
+import QrReader from "react-qr-scanner";
 
 const Deposit = () => {
+  const [delay, setDelay] = useState(100);
+  const [result, setResult] = useState("No Result");
 
-    return (
-        <>
-        </>
-    )
-}
+  const handleScan = data => {
+    setResult(data);
+  };
 
-export default Deposit
+  const handleError = err => {
+    console.error(err);
+  };
+
+  const previewStyle = {
+    height: 100,
+    width: 100
+  };
+
+  return (
+    <div>
+      <QrReader
+        delay={delay}
+        style={previewStyle}
+        onError={handleError}
+        onScan={handleScan}
+      />
+      <p>{result}</p>
+    </div>
+  );
+};
+
+export default Deposit;
