df240d880635acad33f21d4b80232fc0bd35626d
Daniel Lam <dlam795@gmail.com>
2020-02-12 11:04:55 +0800

    login page styled

--- src/components/Modals/LogInForm.js
+++ src/components/Modals/LogInForm.js
@@ -1,8 +1,57 @@
 import React from 'react'
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter, FormFeedback, FormGroup, Label, Input } from 'reactstrap'
+import { Button,Modal, ModalHeader, ModalBody, ModalFooter, FormFeedback, FormGroup, Label, Input } from 'reactstrap'
 import { useHistory } from 'react-router-dom'
 import axios from 'axios'
 import { toast } from 'react-toastify'
+import styled from 'styled-components'
+
+
+
+
+
+const headerstyle={
+    textAlign: 'center',
+    fontSize: '40px',
+    color:'#494949'
+}
+
+const formstyle={
+    textAlign: 'center'
+}
+
+const footerstyle={
+    textAlign: 'center',
+    marginTop:'100px'
+}
+
+const labelstyle={
+    fontSize:'25px',
+
+}
+
+const inputstyle={
+    border: 'none',
+    borderBottom: '1px solid #494949',
+    backgroundColor:'#F7FFFB',
+    width:'200px'
+}
+
+const buttondivstyle={
+    marginTop: '30px',
+    textAlign: 'center'
+}
+
+const buttonstyle={
+    borderRadius: '44px', 
+    border: 'none',
+    width: '332px',
+    height: '48px',
+    backgroundColor: '#B0E6CE',
+    marginTop: '10px',
+    fontSize: '20px',
+    fontFamily: '"Dosis", sans-serif',
+    color: '#494949'
+}
 
 const LogInForm = ({ modal, toggle, isLoginForm, setIsLogInForm, text, setText, password, setPassword, setLoggedIn }) => {
 
@@ -17,7 +66,7 @@ const LogInForm = ({ modal, toggle, isLoginForm, setIsLogInForm, text, setText, 
     }
 
     const isDisabled = () => {
-        return (text === '' || password === '' )
+        return (text === '' || password === '')
     }
 
     const logIn = () => {
@@ -56,25 +105,32 @@ const LogInForm = ({ modal, toggle, isLoginForm, setIsLogInForm, text, setText, 
                 });
             }
             )
-    }
-    
+    } 
+
 
     return (
         <Modal isOpen={modal} toggle={toggle}>
-            <ModalHeader toggle={toggle}>Log In</ModalHeader>
+            <ModalHeader style={headerstyle} toggle={toggle}>Log In</ModalHeader>
             <ModalBody>
-                <form>
-                    <Label>Username</Label>
-                    <Input type="text" autoFocus onChange={handleInput} value={text} />
+                <FormGroup style={formstyle}>
+                    <Label style={labelstyle}>Username</Label><br/>
+                    <Input style={inputstyle} className="bg-transparent" type="text" autoFocus onChange={handleInput} value={text} />
                     <FormFeedback></FormFeedback>
-                    <Label>Password</Label>
-                    <Input type="password" onChange={handlePassword} value={password} />
+                    <Label style={labelstyle}>Password</Label><br/>
+                    <Input style={inputstyle} type="password" onChange={handlePassword} value={password} />
                     <FormFeedback></FormFeedback>
-                </form>
+                </FormGroup>
+
+                <div style={buttondivstyle}>
+                    <Button style={buttonstyle} color="link" onClick={() => setIsLogInForm(!isLoginForm)}>Log In</Button><br/>
+                    <p style={{ marginTop:'35px'}}>or</p><br/>                
+                    <Button style={buttonstyle} color="link" onClick={() => setIsLogInForm(!isLoginForm)}>Log in with Google</Button>
+                </div>
+
             </ModalBody>
-            <ModalFooter>
-                <Button color="primary" onClick={logIn} disabled={isDisabled()}>Log In</Button>{' '}
-                <Button color="link" onClick={() => setIsLogInForm(!isLoginForm)}>Sign Up</Button>
+            <ModalFooter style={footerstyle}>
+                <p>Don't have an account?</p><br/>
+                <link></link>
             </ModalFooter>
         </Modal>
     )
